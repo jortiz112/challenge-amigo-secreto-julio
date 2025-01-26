bc962ba241b7ee8d655a684d4867eb2b83d0815a
jortiz112 <jortiz112@hotmail.com>
2025-01-26 14:47:48 -0800

    Declaración de variable array amigos

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/*El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar 
+  la lógica para resolver el problema.*/
+
+//Declaración de variable de tipo array, que almacenará los nombres de los amigos ingresados
+let amigos = [];
