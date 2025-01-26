b66337700ec929a0951c69197447d9a245e58f66
jortiz112 <jortiz112@hotmail.com>
2025-01-26 15:49:26 -0800

    Validación para asegurar que el campo no este vacío, si está vacío muestra un alert

--- app.js
+++ app.js
@@ -5,12 +5,16 @@
 let amigos = [];
 
 //Función, que permite al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos
-
 function agregarAmigo() {
-    //Se obtiene  el texto del nombre ingresdo por el usuario, del campo de entrada de datos
-
-    let nombre = document.getElementById('amigo').value;
-
-    //
-    console.log(nombre);
+    
+    /*Se añade una validación para evitar que el usuario deje vacío el campo de entrada de datos y muestra un alerta
+      en caso de dejarlo en blanco*/
+    if (document.getElementById('amigo').value === "") {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        //Se obtiene el texto del nombre ingresdo por el usuario, del campo de entrada de datos
+        let nombre = document.getElementById('amigo').value;
+        console.log(nombre);
+    }
+    
 }
