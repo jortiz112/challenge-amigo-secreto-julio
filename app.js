bcc5d3e7344042cb3cdf293d2de121922edb5f6d
jortiz112 <jortiz112@hotmail.com>
2025-01-27 22:06:28 -0800

    Se crea la función actualizaListaAmigos, se obtiene el elemnto, se itera sobre el arreglo, y se agregan los elentos a la lista

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 
 //Declaración de variable de tipo array, que almacenará los nombres de los amigos ingresados
 let amigos = [];
-
+//let contador = 0;
+let lista = "";
 //Función, que permite al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos
 function agregarAmigo() {
     
@@ -17,7 +18,23 @@ function agregarAmigo() {
         amigos.push(document.getElementById('amigo').value);
         //Se restablece el campo de texto a una cadena vacía
         document.getElementById('amigo').value = "";
-        return console.log(amigos);        
-    }
+
+              
+    }  
     
+    return;
+}
+
+/*Función que recorre el array amigos y agrega cada nombre como un elemento <li> dentro de una lista HTML. 
+  se usa innerHTML para limpiar la lista antes de agregar nuevos elementos.*/
+function actualizaListaAmigos() {
+  /*Se usa un bucle for para recorrer el arreglo amigos y crear los elementos de lista (<li>) para cada 
+  nombre y se forma la lsita dentro de la variable lista .*/
+    for(let contador = 0; contador < amigos.length; contador++) {
+      //Para cada amigo, se crea un nuevo elemento de lista.
+      lista += '<li>' + amigos[contador] + '</li>';
+      //Obtengo el elemento de la lista y se ingresa la lista completa en el HTML
+      document.getElementById('listaAmigos').innerHTML = lista;
+    }
+    return;    
 }
